6d13f312c17c8e06ee81dc93b5a74e333fa64614
thomqq <thomqq@gmail.com>
2023-10-08 13:38:14 +0200

    Simplified solution

--- src/TimerCanvas.tsx
+++ src/TimerCanvas.tsx
@@ -17,25 +17,21 @@ const TimerCanvas = ({ width, height }: TimerCanvasProps) => {
     const animRef = useRef<number>(0);
     const running = useRef<boolean>(false);
     const prevTime = useRef<number>(0);
-    const startTime = useRef<number>(0);
-    const loading = useRef<boolean>(false);
-    
+
     const anim = (time : number) => {
         let now = Date.now();
 
-        if( loading.current === true) {
-            if( prevTime.current === 0) {
-                prevTime.current = now;
-                startTime.current = now;
-            }
-
-            let delta = now - prevTime.current;
-
-            if (running.current) {
-                setRunningTime( prev => prev === 0 ? now : prev + delta);
-            } 
+        if( prevTime.current === 0) {
             prevTime.current = now;
         }
+
+        let delta = now - prevTime.current;
+
+        if (running.current) {
+            setRunningTime( prev => prev + delta);
+        } 
+        prevTime.current = now;
+        
         animRef.current =  requestAnimationFrame(anim);
     }
 
@@ -52,8 +48,7 @@ const TimerCanvas = ({ width, height }: TimerCanvasProps) => {
             const ctx = canvas.getContext('2d');
             if (ctx) {
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
-                //console.log("runningTime: " + runningTime + " prevTime: " + prevTime.current + " startTime: " + startTime.current);
-                ctx.fillText( Math.round((runningTime - startTime.current) / 1000 ).toString(), 10, 50);
+                ctx.fillText( Math.round((runningTime) / 1000 ).toString(), 10, 50);
             }
         }
     }, [runningTime])
@@ -63,9 +58,6 @@ const TimerCanvas = ({ width, height }: TimerCanvasProps) => {
         <button onClick={
             () => {
                 running.current = !running.current;
-                if( loading.current === false) {
-                    loading.current = true;
-                }
                 setButtonText(running.current ? "Stop" : "Start");
             }
         }>
