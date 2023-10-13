ed0ba7fc519a85ffc72c3523c6079f9ba055d504
thomqq <thomqq@gmail.com>
2023-10-13 08:42:26 +0200

    Fixed problem with drawing timer

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ function App() {
   return (
     <>
       <div className="App">
-        <TimerCanvas width={100} height={100} items={[ {sec: 20}, {sec:30} ]}/>
+        <TimerCanvas width={100} height={100} items={[ {sec: 20, color : "red"}, {sec:30, color: "green"} ]}/>
       </div>
       <div className="App">
-          <TimerCanvas width={100} height={100} items={[ {sec: 10}, {sec:30} ]}/>
+          <TimerCanvas width={100} height={100} items={[ {sec: 2, color : "red"}, {sec:4, color: "green"}, {sec:2, color: "blue"} ]}/>
       </div>
     </>
   );
--- src/TimerCanvas.tsx
+++ src/TimerCanvas.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from 'react';
 
 type item = {
     sec: number;
+    color: string;
 }
 
 type TimerCanvasProps = {
@@ -24,6 +25,8 @@ const TimerCanvas = ({ width, height, items }: TimerCanvasProps) => {
     const running = useRef<boolean>(false);
     const prevTime = useRef<number>(0);
 
+    const lastItemPosition = useRef<number>(0);
+
     const anim = (time : number) => {
         let now = Date.now();
 
@@ -33,7 +36,7 @@ const TimerCanvas = ({ width, height, items }: TimerCanvasProps) => {
 
         let delta = now - prevTime.current;
 
-        console.log("delta: " + delta + "time: " + time);
+        //console.log("delta: " + delta + "time: " + time);
 
         if (running.current) {
             setRunningTime( prev => prev + delta);
@@ -54,10 +57,11 @@ const TimerCanvas = ({ width, height, items }: TimerCanvasProps) => {
         const canvas = canvasRef.current;
         if (canvas) {
             const ctx = canvas.getContext('2d');
+
             if (ctx) {
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
                 let sec = Math.round((runningTime) / 1000 );
-                let base = items[0].sec; //sec = whole circle
+                let base = sumAllSec( items ); //sec = whole circle
                 let x = canvas.width / 2;
                 let y = canvas.height / 2;
 
@@ -65,11 +69,28 @@ const TimerCanvas = ({ width, height, items }: TimerCanvasProps) => {
                 ctx.lineWidth = 3;
                 ctx.arc(x, y, x - 3, 0, 2 * Math.PI);
                 ctx.stroke();
-                ctx.fillStyle = "#c82124"; //red
-                ctx.beginPath();
-                ctx.arc(x, y, x - 4, - Math.PI / 2 , Math.PI * (sec * 2 / base - 0.5));
-                ctx.lineTo(x, y)
-                ctx.fill();
+
+                let currItemPosion = calcItemPosition(sec, items);
+                let secs = 0;
+                for( let i = 0 ; i < currItemPosion; i++) {
+                    ctx.beginPath();
+                    ctx.fillStyle = items[i].color;
+                    ctx.arc(x, y, x - 4, Math.PI * (secs * 2 / base - 0.5) , Math.PI * ((secs + items[i].sec) * 2 / base - 0.5));
+                    ctx.lineTo(x, y)
+                    ctx.fill();
+                    secs += items[i].sec;                   
+                }
+                
+                let i = currItemPosion;
+                if( i < items.length) {
+                    ctx.beginPath();
+                    ctx.fillStyle = items[i].color;
+                    ctx.arc(x, y, x - 4, Math.PI * (secs * 2 / base - 0.5) , Math.PI * (sec * 2 / base - 0.5));
+                    ctx.lineTo(x, y)
+                    ctx.fill();
+                }
+
+                lastItemPosition.current = currItemPosion;
             }
         }
     }, [runningTime])
@@ -89,3 +110,23 @@ const TimerCanvas = ({ width, height, items }: TimerCanvasProps) => {
 }; 
 
 export default TimerCanvas;
+
+function sumAllSec(items: item[]) {
+    let sum = 0;
+    for (let i = 0; i < items.length; i++) {
+        sum += items[i].sec;
+    }
+    return sum;
+}
+
+function calcItemPosition(sec: number, items: item[]) {
+    let sum = 0;
+    for (let i = 0; i < items.length; i++) {
+        sum += items[i].sec;
+        if (sum >= sec) {
+            return i;
+        }
+    }
+    return items.length;
+}
+
